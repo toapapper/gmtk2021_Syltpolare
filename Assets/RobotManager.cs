2b5c165203e67023c5f3b17a9f63f730d27bc392
JeffPersson <Jeffepeffe@hotmail.com>
2021-06-12 18:47:01 +0200

    Spawners

--- Assets/RobotManager.cs
+++ Assets/RobotManager.cs
@@ -4,47 +4,5 @@ using UnityEngine;
 
 public class RobotManager : MonoBehaviour
 {
-    List<GameObject> robots;
-    List<GameObject> unlockedRobots;
-    List<GameObject> poweredRobots;
-    GameObject lastCheckpoint;
-    GameObject smallRobot;
-    GameObject bigRobot;
-    GameObject reverseRobot;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (robots.Count <= 0 || poweredRobots.Count == 0)
-        {
-            //End Game
-        }
-
-        if (lastCheckpoint != null)
-        {
-            if (smallRobot == null)
-            {
-                CallSpawner(smallRobot);
-            }
-            if (bigRobot == null && unlockedRobots.Contains(bigRobot))
-            {
-                CallSpawner(bigRobot);
-            }
-            if (reverseRobot == null && unlockedRobots.Contains(reverseRobot))
-            {
-                CallSpawner(reverseRobot);
-            }
-        }
-
-
-    }
-    void CallSpawner(GameObject botToSpawn)
-    {
-        lastCheckpoint.GetComponent<Spawner>().SpawnDestroyedRobot(botToSpawn);
-    }  
 }
