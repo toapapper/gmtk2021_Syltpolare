b949cff95e04c4b923042500259bc114bd34334e
Celezt <59172226+Celezt@users.noreply.github.com>
2021-06-13 16:24:55 +0200

    Updated Parallax

--- Assets/Scripts/Other mechanics/Parallax.cs
+++ Assets/Scripts/Other mechanics/Parallax.cs
@@ -4,32 +4,35 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    [SerializeField] private float _parallaxEffect;
+    [SerializeField] private float _parallaxEffectHorizontal = 1;
+    [SerializeField] private float _parallaxEffectVertical = 1;
 
     private Transform _mainCameraTransform;
 
-    private float _length;
-    private float _startPositon;
+    private Vector2 _length;
+    private Vector2 _startPositon;
 
     private void Start()
     {
         _mainCameraTransform = Camera.main.transform;
 
-        _startPositon = transform.position.x;
-        _length = GetComponent<SpriteRenderer>().bounds.size.x;
+        _startPositon = transform.position;
+        _length = GetComponent<SpriteRenderer>().bounds.size;
     }
 
     private void LateUpdate()
     {
-        float temp = _mainCameraTransform.position.x * (1 - _parallaxEffect);
-        float distance = _mainCameraTransform.position.x * _parallaxEffect;
+        Vector3 cameraPosition = _mainCameraTransform.position;
+        float tempHorizontal = cameraPosition.x * (1 - _parallaxEffectHorizontal);
+        float distanceHorizontal = cameraPosition.x * _parallaxEffectHorizontal;
+        float distanceVertical = cameraPosition.y * _parallaxEffectVertical;
 
         Vector3 position = transform.position;
-        transform.position = new Vector3(_startPositon + distance, position.y, position.z);
+        transform.position = new Vector3(_startPositon.x + distanceHorizontal, _startPositon.y + distanceVertical, position.z);
 
-        if (temp > _startPositon + _length)
-            _startPositon += _length;
-        else if (temp < _startPositon - _length)
-            _startPositon -= _length;
+        if (tempHorizontal > _startPositon.x + _length.x)
+            _startPositon.x += _length.x;
+        else if (tempHorizontal < _startPositon.x - _length.x)
+            _startPositon.x -= _length.x;
     }
 }
