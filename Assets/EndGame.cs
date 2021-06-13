48b7672d817b73b74c60c06a4b0b51ab91b622ff
JeffPersson <Jeffepeffe@hotmail.com>
2021-06-13 18:50:37 +0200

    FinalJeff

--- Assets/EndGame.cs
+++ Assets/EndGame.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EndGame : MonoBehaviour
 {
-    SceneLoaderAndController sceneLoaderAndController;
+    public SceneLoaderAndController sceneLoaderAndController;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
