7f587356a05868aa4a5b27e5fa0652152389144b
Celezt <59172226+Celezt@users.noreply.github.com>
2021-06-14 14:35:01 +0200

    Updated slope movement

--- Assets/Scripts/Avatars/Player/PlayerMovement.cs
+++ Assets/Scripts/Avatars/Player/PlayerMovement.cs
@@ -7,30 +7,39 @@ using Celezt.Times;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Move")]
-    [SerializeField] private float _horizontalForce = 4000;
+    [SerializeField] private float _horizontalForceGrounded = 4000;
+    [SerializeField] private float _horizontalForceElevation = 3000;
     [SerializeField] private float _horizontalDrag = 0.1f;
     [Header("Jump")]
     [SerializeField] private float _jumpImpulse = 200;
     [SerializeField] private float _jumpDrag = 0.1f;
     [SerializeField] private float _jumpTime = 0.5f;
     [SerializeField] private int _bodiesOnTopLimit = 1;
-    [SerializeField] private BoxCollider2D _groundCollider;
-    [SerializeField] private LayerMask _groundCollisionMask;
+    //[SerializeField] private BoxCollider2D _groundCollider;
+    [SerializeField] private LayerMask _groundMask;
 
-    private LayerMask _slopeCollisionMask;
-    [SerializeField] private float slopeUpwardsMultiplier = 2.5f;
+    //private LayerMask _slopeCollisionMask;
+    //[SerializeField] private float slopeUpwardsMultiplier = 2.5f;
+    [Header("Slope")]
+    [SerializeField] private float _slopeCheckDistance;
 
     [Header("Fall")]
     [SerializeField] private float _fallDrag = 0.01f;
     [SerializeField] private bool _isFlipped;
 
-
-    private Rigidbody2D _rigidbody2D;
+    private BoxCollider2D _collider;
+    private Rigidbody2D _rigidbody;
     private RigidbodyStack _rigidbodyStack;
+
+    private Vector2 _colliderSize;
+    private Vector2 _slopeNormalPerpendicular;
 
     private Duration _jumpDuration;
 
+    private float _slopeDownAngle;
+    private float _slopeDownAngleOld;
     private float _horizontalValue;
+    private bool _isOnSlope;
     private bool _isJumping;
     private bool _isGrounded;
 
@@ -77,9 +86,12 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<BoxCollider2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
         _rigidbodyStack = GetComponent<RigidbodyStack>();
-        _slopeCollisionMask = LayerMask.GetMask("StaticObstacle");
+        //_slopeCollisionMask = LayerMask.GetMask("StaticObstacle");
+
+        _colliderSize = _collider.size;
     }
 
     private void FixedUpdate()
@@ -87,50 +99,67 @@ public class PlayerMovement : MonoBehaviour
         Vector2 combinedForce = Vector2.zero;
         Vector2 combinedImpulse = Vector2.zero;
 
-        #region slopedetection
-        bool standingOnSlope = false;
-        float slopeAngle = 0;
-        RaycastHit2D leftRC = Physics2D.Raycast(new Vector2(_groundCollider.bounds.min.x, _groundCollider.bounds.max.y), Vector2.down, _groundCollider.size.y, _slopeCollisionMask);
-        RaycastHit2D rightRC = Physics2D.Raycast(new Vector2(_groundCollider.bounds.max.x, _groundCollider.bounds.max.y), Vector2.down, _groundCollider.size.y, _slopeCollisionMask);
+        Vector2 position = transform.position;
 
-        Debug.DrawLine(new Vector2(_groundCollider.bounds.min.x, _groundCollider.bounds.max.y), new Vector2(_groundCollider.bounds.min.x, _groundCollider.bounds.max.y) + Vector2.down * _groundCollider.size.y, Color.red);
-        Debug.DrawLine(new Vector2(_groundCollider.bounds.max.x, _groundCollider.bounds.max.y), new Vector2(_groundCollider.bounds.max.x, _groundCollider.bounds.max.y) + Vector2.down * _groundCollider.size.y, Color.red);
+        //        #region Slope Detection
+        //        bool standingOnSlope = false;
+        //        float slopeAngle = 0;
+        //        RaycastHit2D leftRC = Physics2D.Raycast(new Vector2(_groundCollider.bounds.min.x, _groundCollider.bounds.max.y), Vector2.down, _groundCollider.size.y, _slopeCollisionMask);
+        //        RaycastHit2D rightRC = Physics2D.Raycast(new Vector2(_groundCollider.bounds.max.x, _groundCollider.bounds.max.y), Vector2.down, _groundCollider.size.y, _slopeCollisionMask);
+        //#if UNITY_EDITOR
+        //        Debug.DrawLine(new Vector2(_groundCollider.bounds.min.x, _groundCollider.bounds.max.y), new Vector2(_groundCollider.bounds.min.x, _groundCollider.bounds.max.y) + Vector2.down * _groundCollider.size.y, Color.red);
+        //        Debug.DrawLine(new Vector2(_groundCollider.bounds.max.x, _groundCollider.bounds.max.y), new Vector2(_groundCollider.bounds.max.x, _groundCollider.bounds.max.y) + Vector2.down * _groundCollider.size.y, Color.red);
+        //#endif
+        //        float leftAngle = 0;
+        //        float rightAngle = 0;
 
-        float leftAngle = 0;
-        float rightAngle = 0;
+        //        if (leftRC.collider != null)
+        //            leftAngle = Vector2.SignedAngle(Vector2.up, leftRC.normal);
 
-        if (leftRC.collider != null)
-            leftAngle = Vector2.SignedAngle(Vector2.up, leftRC.normal);
+        //        if (rightRC.collider != null)
+        //            rightAngle = Vector2.SignedAngle(Vector2.up, rightRC.normal);
 
-        if (rightRC.collider != null)
-            rightAngle = Vector2.SignedAngle(Vector2.up, rightRC.normal);
+        //        if(leftAngle != 0)
+        //        {
+        //            standingOnSlope = true;
+        //            slopeAngle = leftAngle;
+        //        }
+        //        else if(rightAngle != 0)
+        //        {
+        //            standingOnSlope = true;
+        //            slopeAngle = rightAngle;
+        //        }
+        //        #endregion
 
-        if(leftAngle != 0)
+
+        //if (!standingOnSlope)
+        //{
+        //    combinedForce += new Vector2(_horizontalValue * _horizontalForce, 0);
+        //}
+        //else if (standingOnSlope)
+        //{
+        //    Vector2 forceToUse = new Vector2();
+        //    forceToUse.x = _horizontalValue * _horizontalForce * Mathf.Cos(slopeAngle * Mathf.Deg2Rad);
+        //    forceToUse.y = _horizontalValue * _horizontalForce * Mathf.Sin(slopeAngle * Mathf.Deg2Rad);
+        //    if (forceToUse.y > 0)//går uppför
+        //        forceToUse.y *= slopeUpwardsMultiplier;
+
+        //    combinedForce += forceToUse;
+        //}
+
+        SlopeCheck(position);
+
+        if (_isGrounded && !_isOnSlope)
         {
-            standingOnSlope = true;
-            slopeAngle = leftAngle;
+            combinedForce += new Vector2(_horizontalValue * _horizontalForceGrounded, 0);
         }
-        else if(rightAngle != 0)
+        else if (_isGrounded && _isOnSlope)
         {
-            standingOnSlope = true;
-            slopeAngle = rightAngle;
+            combinedForce += new Vector2(-_horizontalValue * _horizontalForceGrounded * _slopeNormalPerpendicular.x, -_horizontalValue * _horizontalForceGrounded * _slopeNormalPerpendicular.y);
         }
-        #endregion
-
-
-        if (!standingOnSlope)
+        else if (!_isGrounded)
         {
-            combinedForce += new Vector2(_horizontalValue * _horizontalForce, 0);
-        }
-        else if (standingOnSlope)
-        {
-            Vector2 forceToUse = new Vector2();
-            forceToUse.x = _horizontalValue * _horizontalForce * Mathf.Cos(slopeAngle * Mathf.Deg2Rad);
-            forceToUse.y = _horizontalValue * _horizontalForce * Mathf.Sin(slopeAngle * Mathf.Deg2Rad);
-            if (forceToUse.y > 0)//går uppför
-                forceToUse.y *= slopeUpwardsMultiplier;
-
-            combinedForce += forceToUse;
+            combinedForce += new Vector2(_horizontalValue * _horizontalForceElevation, 0);
         }
 
 
@@ -140,16 +169,52 @@ public class PlayerMovement : MonoBehaviour
             combinedImpulse += new Vector2(0, _jumpImpulse);
         }
 
-        _rigidbody2D.AddRelativeForce(combinedImpulse , ForceMode2D.Impulse);
-        _rigidbody2D.AddRelativeForce(combinedForce * (_isFlipped ? -1 : 1));
-        Drag();
+        _rigidbody.AddRelativeForce(combinedImpulse , ForceMode2D.Impulse);
+        _rigidbody.AddRelativeForce(combinedForce * (_isFlipped ? -1 : 1));
+        ApplyDrag();
     }
 
-    private void Drag()
+    private void ApplyDrag()
     {
-        Vector2 velocity = _rigidbody2D.velocity;
+        Vector2 velocity = _rigidbody.velocity;
         velocity.x *= 1.0f - _horizontalDrag;
         velocity.y *= (_isJumping) ? 1.0f - _jumpDrag : 1.0f - _fallDrag;
-        _rigidbody2D.velocity = velocity;
+        _rigidbody.velocity = velocity;
+    }
+
+    private void SlopeCheck(Vector2 position)
+    {
+        //Vector2 checkPosition = position - new Vector2(0.0f, _colliderSize.y / 2);
+        Vector2 checkPosition = ((_horizontalValue >= 0) ?
+            position + new Vector2(_colliderSize.x / 2, 0) :
+            position - new Vector2(_colliderSize.x / 2, 0))
+            - new Vector2(0.0f, _colliderSize.y / 2);
+
+        SlopeCheckVertical(checkPosition);
+    }
+
+    private void SlopeCheckHorizontal(Vector2 checkPosition)
+    {
+
+    }
+
+    private void SlopeCheckVertical(Vector2 checkPosition)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(checkPosition, Vector2.down, _slopeCheckDistance, _groundMask);
+
+        if (hit)
+        {
+            _slopeNormalPerpendicular = Vector2.Perpendicular(hit.normal).normalized;
+
+            _slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if (_slopeDownAngle != _slopeDownAngleOld)
+                _isOnSlope = true;
+
+            _slopeDownAngleOld = _slopeDownAngle;
+
+            Debug.DrawRay(hit.point, _slopeNormalPerpendicular, Color.red);
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+        }
     }
 }
