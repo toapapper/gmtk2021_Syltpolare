3b7787c2e6941f764a24c40f6b8f534d76af3f9f
Celezt <59172226+Celezt@users.noreply.github.com>
2021-06-17 18:55:51 +0200

    Stabilized the camera

--- Assets/Scripts/Other mechanics/Parallax.cs
+++ Assets/Scripts/Other mechanics/Parallax.cs
@@ -5,10 +5,8 @@ using MyBox;
 
 public class Parallax : MonoBehaviour
 {
-    [SerializeField] private float _parallaxEffectHorizontal = 1;
-    [SerializeField] private float _parallaxEffectVertical = 1;
-    [SerializeField] private bool _usePixelPerfect;
-    [SerializeField, ConditionalField(nameof(_usePixelPerfect)), Min(0)] private int _pixelPerUnit = 16;
+    [SerializeField] private float _parallaxEffectHorizontal = 1.0f;
+    [SerializeField] private float _parallaxEffectVertical = 1.0f;
 
     private Transform _mainCameraTransform;
 
@@ -31,18 +29,11 @@ public class Parallax : MonoBehaviour
         float distanceVertical = cameraPosition.y * _parallaxEffectVertical;
 
         Vector3 position = transform.position;
-        Vector3 newPosition = new Vector3(_startPositon.x + distanceHorizontal, _startPositon.y + distanceVertical, position.z);
-        transform.position = _usePixelPerfect ? PixelPerfectClamp(newPosition, _pixelPerUnit) : newPosition;
+        transform.position = new Vector3(_startPositon.x + distanceHorizontal, _startPositon.y + distanceVertical, position.z);
 
         if (tempHorizontal > _startPositon.x + _length.x)
             _startPositon.x += _length.x;
         else if (tempHorizontal < _startPositon.x - _length.x)
             _startPositon.x -= _length.x;
-    }
-
-    private static Vector3 PixelPerfectClamp(Vector3 locationVector, int pixelsPerUnit)
-    {
-        Vector3 vectorInPixels = new Vector3(Mathf.CeilToInt(locationVector.x * pixelsPerUnit), Mathf.CeilToInt(locationVector.y * pixelsPerUnit), Mathf.CeilToInt(locationVector.z * pixelsPerUnit));
-        return vectorInPixels / pixelsPerUnit;
     }
 }
