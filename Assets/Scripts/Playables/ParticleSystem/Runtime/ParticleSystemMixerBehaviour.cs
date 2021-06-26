7bd14bc11d02bdb0d002f497dabd312c0f74dca7
Celezt <59172226+Celezt@users.noreply.github.com>
2021-06-26 16:24:50 +0200

    Updated particle system track

--- Assets/Scripts/Playables/ParticleSystem/Runtime/ParticleSystemMixerBehaviour.cs
+++ Assets/Scripts/Playables/ParticleSystem/Runtime/ParticleSystemMixerBehaviour.cs
@@ -17,8 +17,13 @@ namespace Celezt.Timeline
             if (!trackBinding)
                 return;
 
-            int inputCount = playable.GetInputCount();
+            var rootPlayable = playable.GetGraph().GetRootPlayable(0);
+            var time = (float)rootPlayable.GetTime();
 
+            if (!Application.isPlaying && !trackBinding.isPlaying)
+                PrepareParticleSystem(playable, trackBinding);
+
+            int inputCount = playable.GetInputCount();
             for (int i = 0; i < inputCount; i++)
             {
                 float inputWeight = playable.GetInputWeight(i);
@@ -31,11 +36,62 @@ namespace Celezt.Timeline
                     isPLaying = true;
                 }
             }
+            if (Application.isPlaying)
+            {
+                if (isPLaying && !trackBinding.isPlaying)
+                    trackBinding.Play();
+                else if (!isPLaying && !trackBinding.isStopped)
+                    trackBinding.Stop();
+            }
+            else
+            {
+                float minDelta = 1.0f / 240;
+                float smallDelta = Mathf.Max(0.1f, Time.fixedDeltaTime * 2);
+                float largeDelta = 0.2f;
 
-            if (isPLaying && !trackBinding.isPlaying)
-                trackBinding.Play();
-            else if (!isPLaying && !trackBinding.isStopped)
-                trackBinding.Stop();
+                if (isPLaying && !trackBinding.isPlaying)
+                    trackBinding.Play();
+                else if (!isPLaying && !trackBinding.isStopped)
+                    trackBinding.Stop();
+
+                if (time < trackBinding.time ||
+                    time > trackBinding.time + largeDelta)
+                {
+                    ResetSimulation(trackBinding, time);
+                }
+                else if (time > trackBinding.time + smallDelta)
+                {
+                    trackBinding.Simulate(time - trackBinding.time, true, false, true);
+                }
+                else if (time > trackBinding.time + minDelta)
+                {
+                    trackBinding.Simulate(time - trackBinding.time, true, false, false);
+                }
+                else
+                    return;
+            }
+        }
+
+        private void ResetSimulation(ParticleSystem particleSystem, float time)
+        {
+            const float maxSimTime = 2.0f / 3;
+
+            if (time < maxSimTime)
+                particleSystem.Simulate(time);
+            else
+            {
+                particleSystem.Simulate(time - maxSimTime, true, true, false);
+                particleSystem.Simulate(maxSimTime, true, false, true);
+            }
+        }
+
+        private void PrepareParticleSystem(Playable playable, ParticleSystem particleSystem)
+        {
+            var rootPlayable = playable.GetGraph().GetRootPlayable(0);
+            var duration = (float)rootPlayable.GetDuration();
+
+            var main = particleSystem.main;
+            if (main.duration < duration) main.duration = duration;
         }
     }
 }
