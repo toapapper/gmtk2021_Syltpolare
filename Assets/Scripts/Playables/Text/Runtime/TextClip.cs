0bf4bb82c3310884a064db7f310a47ec24a297a2
Celezt <59172226+Celezt@users.noreply.github.com>
2021-06-26 13:33:33 +0200

    Added text track

---
+++ Assets/Scripts/Playables/Text/Runtime/TextClip.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace Celezt.Timeline
+{
+    public class TextClip : PlayableAsset, ITimelineClipAsset
+    {
+        public TextBehaviour Template = new TextBehaviour();
+
+        public ClipCaps clipCaps => ClipCaps.Blending;
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            ScriptPlayable<TextBehaviour> playable = ScriptPlayable<TextBehaviour>.Create(graph, Template);
+            return playable;
+        }
+    }
+}
