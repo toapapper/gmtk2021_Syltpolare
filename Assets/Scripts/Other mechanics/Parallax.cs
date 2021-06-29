9e84ab77fbc3d89d2b7f438121280ba2284172d2
Celezt <59172226+Celezt@users.noreply.github.com>
2021-06-29 17:53:32 +0200

    Updated Parallax.cs

--- Assets/Scripts/Other mechanics/Parallax.cs
+++ Assets/Scripts/Other mechanics/Parallax.cs
@@ -9,18 +9,34 @@ public class Parallax : MonoBehaviour
     [SerializeField] private bool _loopBackground = true;
     [SerializeField] private float _parallaxEffectHorizontal = 1.0f;
     [SerializeField] private float _parallaxEffectVertical = 1.0f;
+    [SerializeField] private PivotPosition _pivot = PivotPosition.Transform;
+    [SerializeField, ConditionalField(nameof(_pivot), false, PivotPosition.CustomTransform)] private Transform _targetTransform;
 
     private Transform _mainCameraTransform;
 
-    private Vector2 _length;
-    private Vector2 _startPositon;
+    private Vector3 _length;
+    private Vector3 _pivotPosition;
+
+    private enum PivotPosition
+    {
+        Transform,
+        CustomTransform,
+    }
 
     private void OnEnable()
     {
         _mainCameraTransform = Camera.main.transform;
 
-        _startPositon = transform.position;
-        Debug.Log(transform.position);
+        switch (_pivot)
+        {
+            case PivotPosition.Transform:
+                _pivotPosition = transform.position;
+                break;
+            case PivotPosition.CustomTransform:
+                _pivotPosition = _targetTransform.position;
+                break;
+        }
+
         if (TryGetComponent(out SpriteRenderer spriteRenderer))
             _length = spriteRenderer.bounds.size;
         else if (TryGetComponent(out RectTransform rectTransform))
@@ -35,14 +51,14 @@ public class Parallax : MonoBehaviour
         float distanceVertical = cameraPosition.y * _parallaxEffectVertical;
 
         Vector3 position = transform.position;
-        transform.position = new Vector3(_startPositon.x + distanceHorizontal, _startPositon.y + distanceVertical, position.z);
+        transform.position = new Vector3(_pivotPosition.x + distanceHorizontal, _pivotPosition.y + distanceVertical, position.z);
 
         if (_loopBackground)
         {
-            if (tempHorizontal > _startPositon.x + _length.x)
-                _startPositon.x += _length.x;
-            else if (tempHorizontal < _startPositon.x - _length.x)
-                _startPositon.x -= _length.x;
+            if (tempHorizontal > _pivotPosition.x + _length.x)
+                _pivotPosition.x += _length.x;
+            else if (tempHorizontal < _pivotPosition.x - _length.x)
+                _pivotPosition.x -= _length.x;
         }
     }
 }
